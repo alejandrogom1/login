5659a2150b60165d9dde7a858ee82f6ce37e8e2b
Alejandro Gómez Flores <alex.tipico@gmail.com>
2020-06-19 01:41:49 -0500

    Entrega final

--- LoginRegistroJS/js/index.js
+++ LoginRegistroJS/js/index.js
@@ -1,1 +1,30 @@
-///declarar variables  
+///declarar variables para visualizar objetos del formulario
+ const usuario=document.getElementById('user');
+ const password=document.getElementById('pass');
+ const formulario=document.getElementById('formLogin');
+ //Generando eventos
+ formulario.addEventListener('submit', login);
+// funciones a ejecutar
+function login(e){
+    e.preventDefault();
+   let usuarioVal=usuario.value;
+   let passwordVal=password.value;
+   if(usuarioVal==''|| passwordVal==''){
+    creaMensaje('Verifica tus campos','danger');
+    return;
+   }
+   if(localStorage.getItem('usuario')){
+     let objeto= JSON.parse(localStorage.getItem('usuario'));
+     if(usuarioVal==objeto.user && passwordVal==objeto.pass){
+      creaMensaje('acceso correcto', 'success');
+        localStorage.setItem('sesion','activa');
+       setTimeout(function(){
+              window.open('./inicio.html', '_self');
+         },2000);
+     }else{
+      creaMensaje('acceso incorrecto', 'danger');
+     }
+   }else{
+    creaMensaje('No hay usua', 'danger');
+   }
+}
---
+++ LoginRegistroJS/js/registro.js
@@ -0,0 +1,32 @@
+//declarar variables
+const nombre=document.getElementById('name');
+const email=document.getElementById('email');
+const user=document.getElementById('user');
+const pass=document.getElementById('pass');
+const formulario=document.getElementById('formRegistro');
+formulario.addEventListener('submit', registro);
+function registro(e){
+    e.preventDefault();
+    let nombreVal=nombre.value;
+    let emailVal=email.value;
+    let userVal=user.value;
+    let passVal=pass.value;
+    if(nombreVal==''||emailVal==''||userVal==''||passVal==''){
+        creaMensaje('Revisa tus campos','danger');
+        return;
+    }
+    const usuario={
+        nombre: nombreVal,
+        email:emailVal,
+        user:userVal,
+        pass:passVal
+    }
+  localStorage.setItem('usuario', JSON.stringify(usuario));
+  nombre.value='';
+  email.value='';
+  user.value='';
+  pass.value='';
+  console.log('guardado');
+ creaMensaje('Usuario registrado','success');
+}   
+
